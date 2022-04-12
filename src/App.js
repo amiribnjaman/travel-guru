5e0d33d9cbaa99b7f529af5d8db50c54c280b386
amiribnjaman <amiribnjaman@gmail.com>
2022-04-12 11:45:42 +0600

    Tailwind installed and background img added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Pages/Shared/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h1 style={{margin: 0}}>Nav bar</h1>
+        </div>
+    );
+};
+
+export default NavBar;
